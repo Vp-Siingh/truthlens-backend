06612e52282848e9d75e5ea20fb6004160b922a9
vpsingh <vpstomar95@gmail.com>
2026-05-24 16:34:43 +0530

    Removed dotenv dependency

--- bot.py
+++ bot.py
@@ -14,7 +14,6 @@ CORS(app)
 # TWILIO CONFIG
 # =========================================
 
-load_dotenv()
 
 account_sid = os.getenv("TWILIO_ACCOUNT_SID")
 auth_token = os.getenv("TWILIO_AUTH_TOKEN")
